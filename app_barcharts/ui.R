53a03d43c7fe98e66ac933b5df4eb7e8f462cabf
jonisaac <jonisaac@berkeley.edu>
2020-08-17 22:20:04 -0700

    back to reality

--- app_barcharts/ui.R
+++ app_barcharts/ui.R
@@ -8,57 +8,56 @@ library(plotly)
 library(shinythemes)
 # Define UI for application that draws a histogram
 fluidPage(theme = shinytheme("superhero"), 
-                
-                
-                
-                # Application title
-                titlePanel("Live Data Analysis"),
-                
-                # Sidebar with a slider input for number of bins 
-                flowLayout(
-                  
-                  #sidebarPanel(
-                  # Input: Select a file ----
-                  fileInput("file1", "Choose Data File",
-                            multiple = TRUE,
-                            accept = c("text/csv",
-                                       "text/comma-separated-values,text/plain",
-                                       ".csv")),
-                  # Input: Select a file ----
-                  fileInput("file2", "Choose Search File",
-                            multiple = TRUE,
-                            accept = c("text/csv",
-                                       "text/comma-separated-values,text/plain",
-                                       ".csv")),
-                  # Horizontal line ----
-                  #tags$hr(),
-                  
-                  # Input: Checkbox if file has header ----
-                  #checkboxInput("header", "Header", TRUE),
-                  
-                  # Input: Select separator ----
-                  #radioButtons("sep", "Separator",
-                  #             choices = c(Tilda = "~", 
-                  #                         Comma = ",",
-                  #                         Semicolon = ";",
-                  #                         Tab = "\t"),
-                  #             selected = "~")
-                  #),
-                  
-                  # Main panel for displaying outputs ----
-                  mainPanel(
-                    tabPanel('Plots', 
-                             
-                             
-                             
-                             # Output: Data file ----
-                             plotlyOutput("stacked"),
-                             dataTableOutput("Outliers")
-                    )
-                    
-                  )
-                  
-                  
-                )
-
+          
+          
+          
+          # Application title
+          titlePanel("Live Data Analysis"),
+          
+          # Sidebar with a slider input for number of bins 
+          fluidPage(
+            
+            sidebarPanel(
+              # Input: Select a file ----
+              fileInput("file1", "Choose Data File",
+                        multiple = TRUE,
+                        accept = c("text/csv",
+                                   "text/comma-separated-values,text/plain",
+                                   ".csv")),
+              # Input: Select a file ----
+              fileInput("file2", "Choose Search File",
+                        multiple = TRUE,
+                        accept = c("text/csv",
+                                   "text/comma-separated-values,text/plain",
+                                   ".csv")),
+              # Horizontal line ----
+              tags$hr(),
+              
+              # Input: Checkbox if file has header ----
+              checkboxInput("header", "Header", TRUE),
+              
+              # Input: Select separator ----
+              radioButtons("sep", "Separator",
+                           choices = c(Tilda = "~", 
+                                       Comma = ",",
+                                       Semicolon = ";",
+                                       Tab = "\t"),
+                           selected = "~")
+            ),
+            
+            # Main panel for displaying outputs ----
+            mainPanel(
+              tabPanel('Plots', 
+                       
+                       
+                       
+                       # Output: Data file ----
+                       plotlyOutput("stacked"),
+                       dataTableOutput("Outliers")
+              )
+              
+            )
+            
+            
+          )
 )
