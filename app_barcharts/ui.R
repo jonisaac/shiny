9b0eb18ee16ee767c48df4dbc12b797fa1e50d9e
jonisaac <jonisaac@berkeley.edu>
2020-08-17 21:50:00 -0700

    updated look 2

--- app_barcharts/ui.R
+++ app_barcharts/ui.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 library(shiny)
 library(dplyr)
 library(ggplot2)
@@ -7,7 +7,7 @@ library(readtext)
 library(plotly)
 library(shinythemes)
 # Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("superhero"), 
+fluidPage(theme = shinytheme("superhero"), 
                 
                 
                 
@@ -43,7 +43,7 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                                            Semicolon = ";",
                                            Tab = "\t"),
                                selected = "~")
-                  #),
+                  ),
                   
                   # Main panel for displaying outputs ----
                   mainPanel(
@@ -60,66 +60,4 @@ ui <- fluidPage(theme = shinytheme("superhero"),
                   
                   
                 )
-=======
-library(shiny)
-library(dplyr)
-library(ggplot2)
-library(stringr)
-library(readtext)
-library(plotly)
 
-fluidPage(
-  
-  
-  
-  # Application title
-  titlePanel("Live Data Analysis"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    
-    sidebarPanel(
-      # Input: Select a file ----
-      fileInput("file1", "Choose Data File",
-                multiple = TRUE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      # Input: Select a file ----
-      fileInput("file2", "Choose Search File",
-                multiple = TRUE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      # Horizontal line ----
-      tags$hr(),
-      
-      # Input: Checkbox if file has header ----
-      checkboxInput("header", "Header", TRUE),
-      
-      # Input: Select separator ----
-      radioButtons("sep", "Separator",
-                   choices = c(Tilda = "~", 
-                               Comma = ",",
-                               Semicolon = ";",
-                               Tab = "\t"),
-                   selected = "~")
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      tabPanel('Plots', 
-               
-               
-               
-               # Output: Data file ----
-               plotlyOutput("stacked"),
-               tableOutput("Outliers")
-      )
-      
-    )
-    
-    
-  )
->>>>>>> 3890ad2363f5ea55878f0f1a50bdf59562d6c50d
-)
